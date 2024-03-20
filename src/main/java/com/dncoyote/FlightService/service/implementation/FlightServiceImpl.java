c49e815a6c82a415ee1280752be751413a57d5b4
BilalThaha <bilal.athaha@gmail.com>
2024-03-20 09:42:57 +0530

    commit

--- src/main/java/com/dncoyote/FlightService/service/implementation/FlightServiceImpl.java
+++ src/main/java/com/dncoyote/FlightService/service/implementation/FlightServiceImpl.java
@@ -1,8 +1,11 @@
 package com.dncoyote.FlightService.service.implementation;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -57,8 +60,44 @@ public class FlightServiceImpl implements FlightService {
 
     @Override
     public int getFlightCount(String takeoffTimestamp, String takeoffCity) {
-        return 0;
+        String url = "https://6098f0d799011f001713fbf3.mockapi.io/techcurators/products/flights";
+        Flight[] flights = restTemplate.getForObject(url, Flight[].class);
 
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date parsedDate;
+        try {
+            parsedDate = dateFormat.parse(takeoffTimestamp);
+        } catch (ParseException e) {
+            // Handle parsing exception if needed
+            return 0;
+        }
+
+        long flightCount = Arrays.stream(flights)
+                .filter(flight -> {
+                    Date flightTakeoffDate = flight.getTakeoffTimestamp();
+                    if (flightTakeoffDate == null) {
+                        return false;
+                    }
+                    // Convert flightTakeoffDate to LocalDate for comparison
+                    Date flightDateWithoutTime = removeTimeComponent(flightTakeoffDate);
+                    Date parsedDateWithoutTime = removeTimeComponent(parsedDate);
+                    return flightDateWithoutTime.equals(parsedDateWithoutTime) &&
+                            flight.getDepartureFrom().equalsIgnoreCase(takeoffCity);
+                })
+                .count();
+
+        return (int) flightCount;
+
+    }
+
+    private Date removeTimeComponent(Date date) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            return dateFormat.parse(dateFormat.format(date));
+        } catch (ParseException e) {
+            // Handle parsing exception if needed
+            return null;
+        }
     }
 
 }
